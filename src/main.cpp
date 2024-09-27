2a1ec000377df057d5184676e62f2deb28c281f0
Wassim Akkari <was.akkari@gmail.com>
2024-09-27 16:42:03 +0100

    Using STL std::copy to copy data from std::string to ostream that points to stdout stream (std::cout)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator> // Include this header for std::ostream_iterator
+
+#include "main.h"
+
+template<typename Container>
+void write_to_cout(const Container& container, const char* delimiter = " ")
+{
+    std::copy(container.begin(), container.end(),
+        std::ostream_iterator<typename Container::value_type>(std::cout, delimiter));
+}
 
 int main()
 {
+    std::vector<std::string> a = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+    std::vector<std::string> b = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+    // write initial states
+    write_to_cout(a);
+    std::cout << std::endl;
+    write_to_cout(b);
+    std::cout << std::endl << std::endl;
+
+    // test algorithm
+    std::copy(b.begin(), b.begin() + 4, b.begin() + 6);
+    write_to_cout(b);
+
+    std::cout << std::endl;
+
+    std::cout << typeid(std::vector<std::string>::value_type).name() << std::endl;
 
     std::cin.get();
 }
