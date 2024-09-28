066fa9067b08515c704cc2a1e2257f6d7295a448
Wassim Akkari <was.akkari@gmail.com>
2024-09-28 08:15:10 +0100

    different ways of using std::copy_if with iterators, with std::inserter and finnaly with std::back_inserter, the last to are used to automatically grow the container if necessary and return and iterator ro the space holder for insertion

--- src/main.cpp
+++ src/main.cpp
@@ -7,56 +7,43 @@
 #include "main.h"
 
 template<typename Container>
-void write_to_cout(const Container& container, const char* delimiter = " ")
+void write_to_cout( const Container &container, const char *delimiter = " " )
 {
-    std::copy(container.begin(), container.end(),
-        std::ostream_iterator<typename Container::value_type>(std::cout, delimiter));
+	std::copy( container.begin(), container.end(),
+		std::ostream_iterator<typename Container::value_type>( std::cout, delimiter ) );
 }
 
 int main()
+
 {
-    std::vector<std::string> a = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
-    std::vector<std::string> b = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+	// Init test containers 
+	std::string a = "Happy Fun times With Capital letters";
+	std::string b = "otHErwise KNOwn as upper CASE biotches";
 
-    // write initial states
-    write_to_cout(a);
-    std::cout << std::endl;
-    write_to_cout(b);
-    std::cout << std::endl << std::endl;
+	// Write initial states
+	write_to_cout( a );
+	std::cout << std::endl;
+	write_to_cout( b );
+	std::cout << std::endl;
 
-    // test algorithm
-    std::copy_n(b.begin(), 4, b.begin() + 6); // changing function to copy_n and use number of hopes instead of end iterator
-    write_to_cout(b);
+	// test algorithm
+	std::string uppers;
+	// uppers.resize( a.length() + b.length() );
+	// std::copy_if( a.begin(), a.end(), uppers.begin(), std::isupper );
+	// std::copy_if( b.begin(), b.end(), uppers.begin() + 4, std::isupper );
 
-    std::cout << std::endl;
+	// Inserter is used to grow uppers if necessary and return an iterator of that placeholder
+    // std::copy_if( a.begin(), a.end(), std::inserter(uppers, uppers.begin()), std::isupper);
+	// we use uppers.end() here to not override the previous a string data
+    // std::copy_if( b.begin(), b.end(), std::inserter(uppers, uppers.end()), std::isupper);
 
-    std::cout << typeid(std::vector<std::string>::value_type).name() << std::endl;
+	// an easier way is to just use std::back_inserter to grow and return the last placeholder always
+	// very similar to how push_back does the insertion
+	std::copy_if( a.begin(), a.end(), std::back_inserter( uppers ), std::isupper );
+	std::copy_if( b.begin(), b.end(), std::back_inserter( uppers ), std::isupper );
 
-    // If you want to read from input stream
-    // You cannot give iterator + x with std::copy 
-    // so you have to use std::copy_n instead to read a number of chars
+	// Write result
+	write_to_cout( uppers );
 
-    std::copy_n(std::istream_iterator<std::string>(std::cin), 5, b.begin());
-    std::cout << std::endl;
-    write_to_cout(b);
-    std::cout << std::endl;
-
-    std::vector<std::string> bf = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
-    std::copy_backward(bf.begin() + 1, bf.begin() + 3, bf.begin() + 2); // when you overlapping copy ranges in the same contiguous container
-    // You can achieve the same affect by using rbegin and rend which renders copy_backward as redudant 
-    
-    std::cout << std::endl;
-    write_to_cout(bf);
-    std::cout << std::endl;
-
-    std::vector<std::string> br = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-    std::copy(br.rbegin() + 1, br.rbegin() + 3, br.rbegin() + 2); // when you overlapping copy ranges in the same contiguous container
-    // You can achieve the same affect by using rbegin and rend which renders copy_backward as redudant 
-    
-    std::cout << std::endl;
-    write_to_cout(br);
-    std::cout << std::endl;
-
-    std::cin.get();
+	std::cin.get();
 }
