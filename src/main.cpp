d38527cf54cd3c316f984396b57db813b09d2c0b
Wassim Akkari <was.akkari@gmail.com>
2024-10-09 18:31:02 +0100

    this is a cpp allocation example

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 
 int main()
 {
+	int *num_ptr = nullptr;
+
+	try
+	{
+		num_ptr = new int;
+
+		*num_ptr = 42;
+		std::cout << "Dynamic number: " << *num_ptr << std::endl;
+	}
+	catch ( const std::bad_alloc& e )
+	{
+		std::cout << "Allocation Error: " << e.what() << std::endl;
+		return 1;
+	}
+
+	delete num_ptr;
+
 	return 0;
 }
 
