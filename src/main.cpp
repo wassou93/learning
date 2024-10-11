e5c055233e66c7a5e9cf13fbf216d3b0d59ff4f0
Wassim Akkari <was.akkari@gmail.com>
2024-10-11 14:11:39 +0100

    showcasing why std::forward might be needed to preserve rvalue semantic when passing an rvalue as a param to an inner function, in this instead of creating a special implementation for rvalue that uses std::move internally, you just need to create a templated function with T&& (forward/universal reference) then use std::forward<T>(param) inside to preserve the rvalue/lvalue semantic accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
+#include <iostream>
 
 #include "main.hpp"
 
+struct Object {
+	std::string str;
+	Object( std::string s ) : str( s ) {}
+	// ~Object() = default; // this will delete move semantics implicitly
+};
+
+template<typename T>
+void do_something( T && );
+// void do_something( const Object & );
+// void do_something( Object & );
+// void do_something( Object && );
+void inner_function( const Object & );
+void inner_function( Object & );
+void inner_function( Object && );
+
 int main()
 {
+	const Object obj1{ "object1" };
+	Object obj2 = obj1; // Copy constructor
+
+	obj2.str = "object2";
+	std::cout << obj1.str << std::endl;
+	std::cout << obj2.str << std::endl;
+
+	do_something( obj1 );
+	do_something( obj2 );
+	do_something( Object( "unnamed object" ) );
+
 	return 0;
+}
+
+template<typename T>
+void do_something( T &&var ) {
+	inner_function( std::forward<T>( var ) ); // this will preserve rvalue semantic to keep it from turning into an lvalue because var is a named param
+}
+
+// void do_something( const Object &var ) {
+// 	inner_function( var );
+// }
+// void do_something( Object &var ) {
+// 	inner_function( var );
+// }
+// void do_something( Object &&var ) {
+// 	inner_function( std::move(var) );
+// }
+
+void inner_function( const Object &obj ) {
+	std::cout << "const Object&\n";
+	std::cout << obj.str << std::endl;
+}
+void inner_function( Object &obj ) {
+	std::cout << "Object&\n";
+	std::cout << obj.str << std::endl;
+}
+void inner_function( Object &&obj ) {
+	std::cout << "Object&&\n";
+	std::cout << obj.str << std::endl;
 }
 
