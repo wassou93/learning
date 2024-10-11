de291bc7e835bb2eafc576fd21d71a47dbd3012c
Wassim Akkari <was.akkari@gmail.com>
2024-10-11 11:12:01 +0100

    clear things up

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "main.hpp"
 
 int main()
 {
-	int *num_ptr = ( int * ) malloc( sizeof( int ) );
-
-	if ( num_ptr == NULL )
-	{
-		fprintf( stderr, "Value was not allocated correctly!" );
-		return 1;
-	}
-
-	*num_ptr = 24;
-
-	printf( "%d\n", *num_ptr );
-
-	free( num_ptr );
-
 	return 0;
 }
 
